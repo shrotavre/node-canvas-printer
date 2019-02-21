1f0b9557e2442b65ba6e88d9e23f8214d066d556
shrotavre <avreghly@gmail.com>
2019-02-21 16:46:25 +0700

    Add painter object

---
+++ src/painter.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import path from 'path'
+import puppeteer from 'puppeteer'
+
+const fetchCanvas = (canvasName) => {
+  const canvasPath = path.resolve('./canvases/', `${canvasName}.html`)
+  return fs.readFileSync(canvasPath, 'utf8');
+}
+
+const Painter = async (canvasName) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const canvasCode = fetchCanvas(canvasName)
+
+  await page.setContent(canvasCode);
+
+  const contentElement = await page.$('#canvas-content');
+  await contentElement.screenshot({ path: `results/${Date.now()}.png` });
+
+  await browser.close();
+}
+
+export default Painter
